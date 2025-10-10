aa85fa01e7c2876f66b7500313e93213fbab6b1f
mohamedaasif-web <sulthanasif89@gmail.com>
2025-10-10 21:21:22 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("delete");
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+  taskInput.value = "";
+}
